a691213edd990f0da5ddfb52469de1812f0c01bd
cOueghlani <cristinaoueghlani@gmail.com>
2025-11-17 11:30:33 +0100

    logs

--- app_auth/broker/auth_broker_service.py
+++ app_auth/broker/auth_broker_service.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from microservice_chassis_grupo2.core.rabbitmq_core import get_channel, declare_exchange
+from microservice_chassis_grupo2.core.rabbitmq_core import get_channel, declare_exchange, declare_exchange_logs
 from aio_pika import Message
 
 logger = logging.getLogger(__name__)
@@ -33,11 +33,16 @@ async def publish_auth_status(status: str):
         await exchange.publish(message, routing_key=routing_key)
 
         logger.info(f"📢 Enviado mensaje a RabbitMQ: {message_body}")
+        
     except Exception as e:
         logger.error(f"❌ Error al publicar estado de auth: {e}")
     finally:
         if 'connection' in locals():
             await connection.close()
+    await publish_to_logger(
+        message=message_body,
+        topic=f"auth.info" if status == "running" else "auth.error"
+    )
 
 async def publish_user_registered(user_id: str):
     try:
@@ -63,4 +68,39 @@ async def publish_user_registered(user_id: str):
         logger.error(f"❌ Error al publicar la creacion de un nuevo usuario: {e}")
     finally:
         if "connection" in locals():
+            await connection.close()
+    await publish_to_logger(
+        message={"message": "Nuevo usuario creado", "user_id": user_id},
+        topic="auth.info"
+    )
+
+async def publish_to_logger(message: dict, topic: str):
+    """
+    Envía un log estructurado al sistema de logs.
+    topic: 'auth.info', 'auth.error', 'auth.debug', etc.
+    """
+    connection = None
+    try:
+        connection, channel = await get_channel()
+        exchange = await declare_exchange_logs(channel)
+
+        log_data = {
+            "measurement": "logs",
+            "service": topic.split(".")[0],   # 'auth'
+            "severity": topic.split(".")[1],  # 'info', 'error', etc.
+            **message,
+        }
+
+        msg = Message(
+            body=json.dumps(log_data).encode(),
+            content_type="application/json",
+            delivery_mode=2,
+        )
+
+        await exchange.publish(message=msg, routing_key=topic)
+
+    except Exception as e:
+        print(f"[AUTH] Error publishing to logger: {e}")
+    finally:
+        if connection:
             await connection.close()
